4d3e413ac49ef3e2dbbc7dc7c7f739997299d851
Shawn He <shawnhe1991@gmail.com>
2024-02-11 19:25:21 -0500

    add 60% width

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -7,6 +7,13 @@ export default {
         spaceMono: ["'Space Mono'", "monospace"], // Adding custom font family
         colfax: ['"colfax-web"', "sans-serif"],
       },
+      fontWeight: {
+        // Define a custom name or use a numeric value for the medium font weight
+        medium: 500, // Adding medium font weight
+      },
+      minWidth: {
+        "60p": "60%", // Adding custom minimum width of 60%
+      },
     },
   },
   plugins: [],
