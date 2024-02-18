da2eae253fde4929d7f78deec2629c6653a9e19a
Shawn He <shawnhe1991@gmail.com>
2024-02-18 10:05:46 -0500

    tech stack table added

--- frontend/src/components/TechStack.jsx
+++ frontend/src/components/TechStack.jsx
@@ -1,19 +1,72 @@
 import React from "react";
 import "../../src/output.css"
+import { useReactTable } from "@tanstack/react-table";
 
-export const TechStack = () => {
-  return (
-    <div className= "text-slate-100">
-      <h2 className="text-xl font-medium"> Tech Stack</h2>        
-      <div className="text-base font-normal">
-        <ul>Javascript</ul>
-        <ul>React</ul>
-        <ul>NodeJS</ul>
-        <ul>Ruby</ul>
-        <ul>Python</ul>
-        <ul>CSS</ul>
-        <ul>HTML</ul>
-    </div>
-    </div>
-  )
+const data = [
+  {
+    "Languages" : ["Javascript", "Ruby", "Python", "CSS", "HTML"],
+    "Frameworks" : ["React", "Tailwindcss", "Sass", "JQuery"]
+  }
+]
+
+export const TechStack = (data) => {
+  const table = useReactTable(data)
 }
+
+// export const TechStack = () => {
+//   return (
+//     <div className= "text-black flex flex-row">
+//       <table>
+//               <thead ></thead>
+//               <tr className="text-xl font-medium"> 
+//                 <th>
+//                   TechStack
+//                 </th>
+//                 <th>
+//                   Frameworks
+//                 </th>
+//               </tr>  
+//               <tbody>
+//                  <tr>
+//                     <td>
+//                     Javascript
+//                     </td>
+//                     <td>
+//                     Javascript
+//                     </td>
+//                     <td>
+//                     Javascript
+//                     </td>
+//                     <td>
+//                     Javascript
+//                     </td>
+//                     <td>
+//                     Javascript
+//                     </td>
+//                     <td>
+//                     Javascript
+//                     </td>
+
+//                  </tr>
+//               </tbody>      
+//               <div className="text-base font-normal">
+//                 <ul>Javascript</ul>
+//                 <ul>NodeJS</ul>
+//                 <ul>Ruby</ul>
+//                 <ul>Python</ul>
+//                 <ul>CSS</ul>
+//                 <ul>HTML</ul>
+//               </div>
+
+//               <div className="text-base font-normal">
+//                 <ul>React</ul>
+//                 <ul>Tailwindcss</ul>
+//                 <ul>React</ul>
+//                 <ul>Sass</ul>
+//                 <ul>JQuery</ul>
+//                 <ul>Ajax</ul>
+//               </div>
+//       </table>
+//     </div>
+//   )
+// }
---
+++ frontend/src/stories/TechStack.stories.js
@@ -0,0 +1,13 @@
+import { TechStack } from "../components/TechStack";
+
+export default {
+  title: "TechStack",
+  component: TechStack,
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: "TechStack",
+  },
+};
