e009b5cb1dbe8c1167e751693e356b485a9fe24f
Shawn He <shawnhe1991@gmail.com>
2024-02-27 12:29:45 -0500

    added compontent to accomodate the radial gradient effect

---
+++ frontend/src/components/Unified.jsx
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from "react"
+import App from "../App";
+import { Navbar } from "./Navbar";
+import '../output.css'
+
+
+export const MainContainer = () => {
+  const [gradientStyle, setGradientStyle] = useState({});
+
+  const handleMouseMove = (e) => {
+    const x = e.pageX - e.currentTarget.offsetLeft;
+    const y = e.pageY - e.currentTarget.offsetTop;
+    const gradient = `radial-gradient(400px circle at ${x}px ${y}px, rgba(56, 189, 248, 0.15), transparent 80%)`;
+    setGradientStyle({ background: gradient });
+  };
+
+  const handleMouseLeave = () => {
+    setGradientStyle({});
+  };
+
+  return (
+    <div style={gradientStyle} onMouseMove={handleMouseMove} onMouseLeave={handleMouseLeave}>
+      <Navbar gradientStyle={gradientStyle}/>
+      <App gradientStyle={gradientStyle}/>
+    </div>
+  );
+}
---
+++ frontend/src/stories/Experiences-tailwind.stories.js
@@ -0,0 +1,13 @@
+import { Experiences } from "../components/Experiences";
+
+export default {
+  title: "Experiences",
+  component: Experiences,
+};
+
+export const Primary = {
+  args: {
+    primary: true,
+    label: "Experiences",
+  },
+};
