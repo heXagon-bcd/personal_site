a0f360e378f848bfdf6fb66c5620dd4e093e3522
Shawn He <shawnhe1991@gmail.com>
2024-04-12 16:23:52 -0400

    refactor

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,9 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react-scroll"],
+  resolve: {
+    dedupe: ["react", "react-dom", "react-scroll"],
+    alias: {
+      "react-scroll": "/node_modules/react-scroll/modules/index.js", // adjust this path based on actual location
     },
   },
 });
