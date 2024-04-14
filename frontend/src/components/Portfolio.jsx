7e8738ee88548179bcaba681ba839ace9e41e45b
Shawn He <shawnhe1991@gmail.com>
2024-04-14 17:50:40 -0400

    add link icon

--- frontend/src/components/Portfolio.jsx
+++ frontend/src/components/Portfolio.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import '../../src/output.css'
+import { IoLink } from "react-icons/io5";
+import '../../src/output.css';
 
 const data = [
   {image:"/images/portfolio_images/flyboy.png",
@@ -39,19 +40,21 @@ export const Portfolio = () => {
     <div className="grid grid-cols-1 font-semibold text-white md:grid-cols-2 ">
       {
         data.map((item,index) => (
-          <div className="flex">
-            <div index={index} className="flex flex-col p-3 mb-12 border-2 rounded-md h-96 md:h-72 md:mr-3 md:w-11/12 md:flex-row font-colfax hover:border-fuchsia-300 hover:shadow-fuchsia-300 hover:shadow-md hover:bg-white/5 backdrop-blur-3xl">
-                <div className=" md:w-5/6">
-                  <div href={item.url} className="flex justify-center mb-4 text-2xl text-slate-100">{item.title}</div>
-                  <div className="flex mb-2 text-sm text-center md:h-32 md:text-left text-slate-300">{item.description}</div>
-                  <div className="flex flex-row flex-wrap justify-center md:justify-normal">{item.technology.map((tech, index) => (
-                    <div key={index} className="mx-1 w-1.65"><span className="px-3 py-1 text-xs font-semibold text-purple-400 bg-purple-400 rounded-full bg-opacity-20">{tech}</span></div>
-                    ))}
-                  </div>
-                </div>  
-                <div className="flex items-center justify-center order-first h-24 m-5 mb-4 md:w-1/6 md:m-0 md:order-last"><img className="h-20" src={item.image} alt={item.title}/></div>         
+          <a href={item.url}>
+            <div className="flex">
+              <div index={index} className="flex flex-col p-3 mb-12 border-2 rounded-md h-96 md:h-72 md:mr-3 md:w-11/12 md:flex-row font-colfax hover:border-fuchsia-300 hover:shadow-fuchsia-300 hover:shadow-md hover:bg-white/5 backdrop-blur-3xl">
+                  <div className=" md:w-5/6">
+                    <div href={item.url} className="flex justify-center mb-4 text-2xl text-slate-100">{item.title}  <IoLink className="ml-4"/></div>
+                    <div className="flex mb-2 text-sm text-center md:h-32 md:text-left text-slate-300">{item.description}</div>
+                    <div className="flex flex-row flex-wrap justify-center md:justify-normal">{item.technology.map((tech, index) => (
+                      <div key={index} className="mx-1 w-1.65"><span className="px-3 py-1 text-xs font-semibold text-purple-400 bg-purple-400 rounded-full bg-opacity-20">{tech}</span></div>
+                      ))}
+                    </div>
+                  </div>  
+                  <div className="flex items-center justify-center order-first h-24 m-5 mb-4 md:w-1/6 md:m-0 md:mt-10 md:order-last"><img className="h-20" src={item.image} alt={item.title}/></div>         
+              </div>
             </div>
-          </div>
+          </a>
         ))
       }
     </div>
